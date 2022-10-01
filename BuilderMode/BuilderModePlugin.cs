9e6cb79d8c4e3e9432c0a329d7aef9c949643470
ElMargaritox <alexisguzman0303@gmail.com>
2022-10-01 15:45:04 -0300

    Add project files.

---
+++ BuilderMode/BuilderModePlugin.cs
@@ -0,0 +1,93 @@
+﻿using Rocket.Core.Logging;
+using Rocket.Core.Plugins;
+using Rocket.Unturned.Events;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using Steamworks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderMode
+{
+    public class BuilderModePlugin : RocketPlugin
+    {
+        public static BuilderModePlugin Instance { get; private set; }
+        protected override void Load()
+        {
+            Instance = this;
+            Logger.Log($"Plugin By Margarita#8172", ConsoleColor.Yellow);
+
+            StructureManager.onStructureSpawned += OnStructureSpawned;
+            BarricadeManager.onBarricadeSpawned += OnBarricadeSpawned;
+
+            StructureDrop.OnSalvageRequested_Global += StructureDrop_OnSalvageRequested_Global;
+            BarricadeDrop.OnSalvageRequested_Global += BarricadeDrop_OnSalvageRequested_Global;
+        }
+
+        private void BarricadeDrop_OnSalvageRequested_Global(BarricadeDrop barricade, SteamPlayer instigatorClient, ref bool shouldAllow)
+        {
+
+            if (this.Builders.Contains(barricade.GetServersideData().owner))
+            {
+
+                UnturnedPlayer player = UnturnedPlayer.FromSteamPlayer(instigatorClient);
+
+                List<InventorySearch> list = player.Inventory.search(barricade.asset.id, true, true);
+
+                if (list.Count > 1) player.Inventory.removeItem(list[0].page, player.Inventory.getIndex(list[0].page, list[0].jar.x, list[0].jar.y));
+            }
+
+        }
+
+        private void StructureDrop_OnSalvageRequested_Global(StructureDrop structure, SteamPlayer instigatorClient, ref bool shouldAllow)
+        {
+
+            if (this.Builders.Contains(structure.GetServersideData().owner))
+            {
+                UnturnedPlayer player = UnturnedPlayer.FromSteamPlayer(instigatorClient);
+
+                List<InventorySearch> list = player.Inventory.search(structure.asset.id, true, true);
+
+                if (list.Count > 1) player.Inventory.removeItem(list[0].page, player.Inventory.getIndex(list[0].page, list[0].jar.x, list[0].jar.y));
+            }
+
+        }
+
+        private void OnBarricadeSpawned(BarricadeRegion region, BarricadeDrop drop)
+        {
+            if (this.Builders.Contains(drop.GetServersideData().owner))
+            {
+                UnturnedPlayer unturnedPlayer = UnturnedPlayer.FromCSteamID(new CSteamID(drop.GetServersideData().owner));
+                if (unturnedPlayer != null)
+                {
+                    unturnedPlayer.GiveItem(drop.asset.id, 1);
+                }
+            }
+        }
+
+        private void OnStructureSpawned(StructureRegion region, StructureDrop drop)
+        {
+            if (this.Builders.Contains(drop.GetServersideData().owner))
+            {
+                UnturnedPlayer unturnedPlayer = UnturnedPlayer.FromCSteamID(new CSteamID(drop.GetServersideData().owner));
+                if (unturnedPlayer != null)
+                {
+                    unturnedPlayer.GiveItem(drop.asset.id, 1);
+                }
+            }
+        }
+
+        protected override void Unload()
+        {
+            StructureManager.onStructureSpawned -= OnStructureSpawned;
+            BarricadeManager.onBarricadeSpawned -= OnBarricadeSpawned;
+            StructureDrop.OnSalvageRequested_Global -= StructureDrop_OnSalvageRequested_Global;
+            BarricadeDrop.OnSalvageRequested_Global -= BarricadeDrop_OnSalvageRequested_Global;
+        }
+
+        public List<ulong> Builders = new List<ulong>();
+    }
+}
---
+++ BuilderMode/Commands/CommandBuilderMode.cs
@@ -0,0 +1,71 @@
+﻿using Rocket.API;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Player;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderMode.Commands
+{
+    internal class CommandBuilderMode : IRocketCommand
+    {
+        public AllowedCaller AllowedCaller => AllowedCaller.Player;
+
+        public string Name => "buildermode";
+
+        public string Help => "Enable/Disable BuilderMode";
+
+        public string Syntax => "/buildermode <on>/<off>";
+
+        public List<string> Aliases => new List<string>();
+
+        public List<string> Permissions => new List<string>();
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            UnturnedPlayer player = (UnturnedPlayer)caller;
+
+            var instance = BuilderModePlugin.Instance;
+
+            if (command.Length < 1 || command.Length >= 2)
+            {
+                UnturnedChat.Say(caller, $"Use: {this.Syntax}");
+                return;
+            }
+
+            switch (command[0].ToLower())
+            {
+                case "on":
+                    if (instance.Builders.Contains(player.CSteamID.m_SteamID))
+                    {
+                        UnturnedChat.Say(caller, "You are in mode builder");
+                    }
+                    else
+                    {
+                        UnturnedChat.Say(caller, "Enabled builder mode");
+                        instance.Builders.Add(player.CSteamID.m_SteamID);
+                    }
+                    break;
+                case "off":
+                    if (instance.Builders.Contains(player.CSteamID.m_SteamID))
+                    {
+                        instance.Builders.Remove(player.CSteamID.m_SteamID);
+                        UnturnedChat.Say(caller, "Disabled builder mode");
+                    }
+                    else
+                    {
+                        UnturnedChat.Say(caller, "You are not in mode builder");
+
+                    }
+                    break;
+                default:
+                    UnturnedChat.Say(caller, $"Use: {this.Syntax}");
+                    break;
+            }
+
+
+        }
+    }
+}
